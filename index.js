12b8b279a1d000b9fda8bc599782d32a3ea3df99
Collins <mutaic84@gmail.com>
2022-04-23 13:08:17 +0300

    create a static definition method

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ class Tree {
     constructor(species) {
         this.species = species;
     }
+    static definnition() {
+        return `A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.`
+    }
 }
